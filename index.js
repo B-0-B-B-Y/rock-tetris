5ad88aef35edb444272a0494e64d1fc8b38dc463
Dan Foad <danielfoad@gmail.com>
2018-01-12 01:27:53 +0000

    Added index js and main files

---
+++ index.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+app.on('ready', () => {
+    let win = new BrowserWindow({
+        height: 800,
+        width: 500,
+    })
+
+    win.loadURL(path.join(__dirname, '/app/index.html'))
+})
