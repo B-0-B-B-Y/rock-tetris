e1948c5fed01f063eb72fc3d251a2ed589a02731
Bob Naydenov <bob.naydenov@hotmail.com>
2018-01-12 23:43:08 +0000

    Add bubble effects to page

--- app/script/index.js
+++ app/script/index.js
@@ -523,3 +523,23 @@ function selectRandom(){
 
 selectRandom();
 player.play();
+
+// Add particle effects to main page
+
+function bubbles() {
+   $.each($(".particletext.bubbles"), function(){
+      var bubblecount = ($(this).width()/50)*10;
+      for(var i = 0; i <= bubblecount; i++) {
+         var size = ($.rnd(40,80)/10);
+         $(this).append('<span class="particle" style="top:' + $.rnd(20,80) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+      }
+   });
+}
+
+jQuery.rnd = function(m,n) {
+      m = parseInt(m);
+      n = parseInt(n);
+      return Math.floor( Math.random() * (n - m + 1) ) + m;
+}
+
+bubbles()
