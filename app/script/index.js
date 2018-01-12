43011ebd088a33e101a5c8cc72374a95d5e4181e
Bob Naydenov <bob.naydenov@hotmail.com>
2018-01-12 16:31:46 +0000

    Added all render functions

--- app/script/index.js
+++ app/script/index.js
@@ -257,3 +257,67 @@ function invalidateNext() {
 function invalidateScore() {
   invalid.score = true
 }
+
+function invalidateRows() {
+  invalid.rows = true
+}
+
+function draw() {
+  ctx.save()
+  ctx.lineWidth = 1
+  ctx.translate(0.5, 0.5)
+  drawCourt()
+  drawNext()
+  drawScore()
+  drawRows()
+  ctx.restore()
+}
+
+function drawCourt() {
+  if (invalid.court) {ctx.clearRect(0, 0, canvas.width, canvas.height)
+  if (playing)
+    drawPiece(ctx, current.type, current.x, current.y, current.dir)
+  var x, y, block
+  for(y = 0; y < ny; y++) {
+    for(x = 0; x < nx; x++) {
+      if (block = getBlock(x, y))
+        drawBlock(ctx, x, y, block.color)
+    }
+  }
+  ctx.stroke(0, 0, nx*dx - 1, ny*dy -1)   // NB: This is the court boundary
+  invalid.court = false
+  }
+}
+
+function drawNext() {
+  if (invalid.next) {
+    var paddding = (nu - next.type.size) / 2 // Center the piece
+    uctx.save()
+    uctx.translate(0.5, 0.5)
+    uctx.clearRect(0, 0, nu*dx, nu*dy)
+    drawPiece(uctx, next.type, padding, padding, next.dir)
+    uctx.strokeStyle = 'black'
+    uctx.strokeRect(0, 0, nu*dx - 1, nu*dy -1)
+    uctx.restore()
+    invalid.next = false
+  }
+}
+
+function drawScore() {
+  if (invalid.score) {
+    html('score', ("00000" + Math.floor(score)).slice(-5))
+    invalid.score = false
+  }
+}
+
+function drawPiece(ctx, tpye, x, y, dir) {
+  eachblock(type, x, y, dir, function(x, y) {
+    drawBlock(ctx, x, y, type.color)
+  })
+}
+
+function drawBlock(ctx, x, y, color) {
+  ctx.fillStyle = color
+  ctx.fillRect(x*dx, y*dy, dx, dy)
+  ctx.strokeRect(x*dx, y*dy, dx, dy)
+}
