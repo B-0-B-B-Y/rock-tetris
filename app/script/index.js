dae8659426831dc28c96a40e5b69fec914f8c803
Dan Foad <danielfoad@gmail.com>
2018-01-12 18:24:51 +0000

    Grid actually matches size

--- app/script/index.js
+++ app/script/index.js
@@ -74,16 +74,19 @@ var KEY     = { ESC: 27, SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 },
     uctx    = ucanvas.getContext('2d'),
     speed   = { start: 0.6, decrement: 0.005, min: 0.1 }, // seconds until current piece drops 1 row
     nx      = 10,                                         // width of tetris court (in blocks)
-    ny,                                         // height of tetris court (in blocks)
+    ny      = 20,                                         // height of tetris court (in blocks)
     nu      = 16                                          // width/height of upcoming preview (in blocks)
 
 
 // Create the variables that will most likely reset for every games
 
-var height = canvas.offsetHeight,
-    width  = canvas.offsetWidth
+var height = canvas.scrollHeight,
+    width  = canvas.scrollWidth
 
-var dx = width / nx, dy = width / nx,        // pixel size of a single tetris block
+canvas.width = width
+canvas.height = height
+
+var dx = width / nx, dy = height / ny,        // pixel size of a single tetris block
     blocks,        // 2 dimensional array (nx*ny) representing tetris court - either empty block or occupied by a 'piece'
     actions = [],       // queue of user actions (inputs)
     playing = true,       // true|false - game is in progress
@@ -94,10 +97,6 @@ var dx = width / nx, dy = width / nx,        // pixel size of a single tetris bl
     rows,          // number of completed rows in the current game
     step          // how long before current piece drops by 1 row
     pace = 2;
-
-ny = dy * 20
-
-console.log(width)
 
 // Define get and set methods to variables
 
