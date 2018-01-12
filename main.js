54bebd46dff92bd4af401e0736e597500e7641fa
Bob Naydenov <bob.naydenov@hotmail.com>
2018-01-12 01:59:02 +0000

    Add icon setting in main.js

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ app.on('ready', () => {
         height: height / 1.5,
         width: width / 2.5,
         frame: false,
+        icon: path.join(__dirname, 'app/build/512x512.png')
     })
 
     win.loadURL(path.join(__dirname, '/app/index.html'))
